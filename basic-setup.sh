4a1f4894333307fe958a8036ec7170324509d050
lcraaaa <andrew_lcr@icloud.com>
2021-03-25 22:01:18 +0800

    script to install homebrew and change default shell

--- basic-setup.sh
+++ basic-setup.sh
@@ -1,15 +1,67 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+## set location
+while true
+do
+  read -p "Are you in China? (y/n) "
+  if [[ $REPLY == "y" ]]
+  then
+    echo "set location to China"
+    AREA="cn"
+    break
+  elif [[ $REPLY == "n" ]]
+  then
+    echo "set location to other"
+    AREA="other"
+    break
+  else
+    echo "invalid answer"
+    continue
+  fi
+done
+
+## Change default shell to /bin/zsh
+if [[ $SHELL == "/bin/zsh" ]]
+then
+  echo "Check Complete"
+else
+  echo $SHELL
+  echo "Change to zsh shell"
+  chsh -s /bin/zsh
+  if [[ $? == 0 ]]
+  then
+    echo "Change shell sucessfully"
+  else
+    echo "Unsucessful"
+  fi
+fi
+
+## install apple command line tool
+xcode-select --install
 
 ## Install package manager home brew
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+if [[ $(command -v brew) == "" ]]
+then
+  echo "installing homebrew"
+  if [[ $AREA == "cn" ]]
+  then
+    echo "install homebrew from china mirror repo"
+    /bin/zsh -c "$(curl -fsSL https://gitee.com/cunkai/HomebrewCN/raw/master/Homebrew.sh)"
+  elif [[ $AREA == "other" ]]
+   then
+    echo "install homebrew from origin repo"
+    /bin/zsh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+  echo "home brew is installed"
+else
+  echo "homebrew is installed"
+fi
 
 ## Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 ## change shell to dafault to zsh
-chsh -s /bin/zsh
+
 
 ## Install useful software
 # jump: fast navigation of file
-brew install jump
-echo eval "$(jump shell --bind=goto)" >> ~/.zshrc
